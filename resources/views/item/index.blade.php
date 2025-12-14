bb574618bbbd0c853342557b3dcd890d494fd3fb
Greg Shenefelt <60129025+greg0rys@users.noreply.github.com>
2025-12-13 18:34:18 -0800

    adding user item realtions views

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -47,7 +47,7 @@
                 <td>{{$it->asset_id}}</td>
                 <td> {{$it->name}} </td>
                 <td>{{$it->item_type->name}}</td>
-                <td>{{$it->user->full_name}}</td>
+                <td><a href="{{ route('users.items', $it->user->id) }}">{{ $it->user->full_name }}</a></td>
                 <td>{{$it->company_location->name}}</td>
                 <td>{{$it->company_location->id}}</td>
             </tr>
